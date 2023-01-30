a595192d5c1ecb44846d14761f21da282eeb52d3
AmiraMandi <amiramandisbaa@gmail.com>
2023-01-30 20:07:43 +0000

    Landing page with React

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,34 @@
 import React from "react";
-
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+import NavBar from "./NavBar.jsx";
+import Cards from "./Cards.jsx";
+import Jumbotron from "./Jumbotron.jsx";
+import Footer from "./Footer.jsx";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+		<NavBar />
+			<div className="container">
+
+				<Jumbotron />
+				<div className="row">
+					<div className="row row-cols-1 row-cols-md-4 row-cols-sm-6 mb-3">
+						<Cards cardsTitle="Card Tittle"cardsText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati officiis earum delectus ipsam aliquid esse libero. Repellat, earum officiis quos molestiae nihil praesentium recusandae ipsam ratione, error a, tempore nostrum."/>
+						<Cards cardsTitle="Card Tittle"cardsText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati officiis earum delectus ipsam aliquid esse libero. Repellat, earum officiis quos molestiae nihil praesentium recusandae ipsam ratione, error a, tempore nostrum."/>
+						<Cards cardsTitle="Card Tittle"cardsText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati officiis earum delectus ipsam aliquid esse libero. Repellat, earum officiis quos molestiae nihil praesentium recusandae ipsam ratione, error a, tempore nostrum."/>
+						<Cards cardsTitle="Card Tittle"cardsText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati officiis earum delectus ipsam aliquid esse libero. Repellat, earum officiis quos molestiae nihil praesentium recusandae ipsam ratione, error a, tempore nostrum."/>
+
+					</div>
+
+
+				</div>
+
+			</div>
+		<Footer/>
 		</div>
-	);
+
+			);
 };
 
-export default Home;
+			export default Home;
